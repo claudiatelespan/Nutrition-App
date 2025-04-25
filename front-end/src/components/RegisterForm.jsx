e4326239af26d528f4c5b8bfbaa79c7f38fa3586
Claudia Telespan <claudiatelespan@gmail.com>
2025-04-25 22:58:31 +0300

    stilizare register

--- front-end/src/components/RegisterForm.jsx
+++ front-end/src/components/RegisterForm.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import { register } from '../services/authService';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 
 export default function RegisterForm() {
   const [formData, setFormData] = useState({ username: '', email: '', password: '' });
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -14,6 +18,7 @@ export default function RegisterForm() {
     try {
       const response = await register(formData);
       console.log("Register successful", response);
+      navigate("/");
       setError("");
     } catch (err) {
       setError("Register failed. Try again.");
@@ -22,6 +27,13 @@ export default function RegisterForm() {
 
   return (
     <form onSubmit={handleSubmit} className="register-form">
+      
+      <div className="py-3">
+        <center>
+          <span className="text-2xl font-semibold">Create your account</span>
+        </center>
+      </div>
+      
       <input
         name="username"
         type="text"
@@ -29,7 +41,9 @@ export default function RegisterForm() {
         value={formData.username}
         onChange={handleChange}
         required
+        className="block font-medium text-gray-700 w-full rounded-md py-2.5 px-4 border border-gray-300 text-sm outline-[#f84525]"
       />
+
       <input
         name="email"
         type="email"
@@ -37,19 +51,41 @@ export default function RegisterForm() {
         value={formData.email}
         onChange={handleChange}
         required
+        className="mt-4 block font-medium text-gray-700 w-full rounded-md py-2.5 px-4 border border-gray-300 text-sm outline-[#f84525]"
       />
-      <input
-        name="password"
-        type="password"
-        placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
-        required
-      />
+
+      <div className='mt-4 relative'>
+        <input
+          name="password"
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+          className="block font-medium text-gray-700 w-full rounded-md py-2.5 px-4 border border-gray-300 text-sm outline-[#f84525]"
+        />
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+          aria-label={showPassword ? "Hide password" : "Show password"}
+        >
+          {showPassword ? (
+            <EyeSlashIcon className="h-5 w-5" />
+          ) : (
+            <EyeIcon className="h-5 w-5" />
+          )}
+        </button>
+      </div>
+
       {error && <p className="text-red-500">{error}</p>}
-      <button type="submit" className="w-full bg-green-600 text-white p-2 rounded">
-        Register
-      </button>
+      
+      <div className='flex items-center justify-center mt-4'>
+        <button type="submit" className="ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+          Register
+        </button>
+      </div>
+
     </form>
   );
 }
--- front-end/src/pages/RegisterPage.jsx
+++ front-end/src/pages/RegisterPage.jsx
@@ -1,9 +1,29 @@
 import RegisterForm from "../components/RegisterForm";
+import { Link } from 'react-router-dom';
 
 export default function RegisterPage() {
   return (
-    <div>
-      <RegisterForm />
+    <div className="font-sans text-gray-900 antialiased">
+      <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#f8f4f3]">
+        
+        <div>
+            <a href="/register">
+              <h2 class="font-bold text-3xl">Nutri<span class="bg-[#f84525] text-white px-2 rounded-md">App</span></h2>
+            </a>
+        </div>
+        
+        <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+          <RegisterForm/>
+        </div>
+
+        <h3 className="mt-4 text-gray-700">
+          Already have an account?{" "}
+          <Link to="/" className="text-[#f84525] font-semibold hover:underline">
+            Sign in
+          </Link>
+        </h3>
+
+      </div>
     </div>
   );
 }
