d548d6f4cd57e76f0471e089fe58ca42439f4a88
Claudia Telespan <claudiatelespan@gmail.com>
2025-05-19 00:09:22 +0300

    adaugare imagini

--- back-end/recipes/management/commands/populate_recipes.py
+++ back-end/recipes/management/commands/populate_recipes.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.core.files import File
 from recipes.models import Ingredient, Recipe, RecipeIngredient
 import json
 import os
@@ -17,6 +18,8 @@ class Command(BaseCommand):
             with open(f"./recipes/management/data/{cat}_augmented.json", "r", encoding="utf-8") as f:
                 recipes_data = json.load(f)
             for recipe in recipes_data:
+                image_file_name = recipe["name"].replace('"','').replace(' ', '_')
+                image_path = f"./recipes/management/data/images/{image_file_name}.jpg"
                 reteta = Recipe.objects.create(
                     name=recipe["name"],
                     description=recipe["summary"],
@@ -33,7 +36,12 @@ class Command(BaseCommand):
                     difficulty=recipe["difficulty"],
                     category=recipe["category"],
                     rating=recipe["rating"],
-                    image=""
                 )
+                try:
+                    with open(image_path, "rb") as img_file:
+                        reteta.image.save(f"{image_file_name}.jpg", File(img_file), save=True)
+                except FileNotFoundError:
+                    print(f"⚠️ Imaginea nu a fost găsită pentru {recipe['name']}")
 
+                
         self.stdout.write(self.style.SUCCESS("✔️ Baza de date a fost populată cu retete!"))
